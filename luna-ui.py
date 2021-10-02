933b9ed924a1ff68778bf1b5c5ab8d3fc7459a00
Tomas Vitha <fakir@hotmail.cz>
2021-10-02 15:12:45 +0200

    Fixed gpsd poller crash and added some logging

--- luna-ui.py
+++ luna-ui.py
@@ -3,6 +3,7 @@ import time
 import atexit
 import luna_hardware
 import luna_core
+import logging
 import netifaces as ni
 
 # get hardware objects
@@ -13,11 +14,15 @@ led = luna_hardware.led
 srvGpsd = luna_core.LinuxService('gpsd')
 srvChrony = luna_core.LinuxService('chrony')
 
+# get logger
+logger = logging.getLogger(__name__)
+logger.addHandler(luna_core.file_handler)
+
 # clear the LCD and LED on exit
-def clearHardware():
-    lcd.clear()
-    led.value = False
-atexit.register(clearHardware)
+def clear_hw():
+  lcd.clear()
+  led.value = False
+atexit.register(clear_hw)
 
 # LED test and welcome screen
 time.sleep(0.1)
@@ -30,33 +35,47 @@ led.value = False
 # Show hostname and IP address
 ipAddress = ni.ifaddresses('eth0')[ni.AF_INET][0]['addr']
 lcd.clear()
+lcd.home()
 lcd.message = 'luna.vitha.cz\n' + ipAddress
 time.sleep(4)
 lcd.clear()
 
 # Prepare GPS strings
+def get_gps_status():
+    if srvGpsd.IsRunning():
+        fix_type = luna_core.getGpsdFixType()
+        text = ''
+        led_status = False
+        if fix_type == 1:
+            text = 'GPS NO FIX'
+            led_status = False
+        elif fix_type == 2:
+            text = 'GPS 2D    '
+            led_status = not led.value # blink at polling rate
+        elif fix_type == 3:
+            text = 'GPS 3D    '
+            led_status = True
+    else:
+        text = 'NO GPS    '
+        led_status = False
+    return text, led_status
 
-
+  
 
 # show clock (temp)
 while True:
-    t = time.localtime()
-    strTime = time.strftime("%H:%M:%S", t)
+  t = time.localtime()
+  strTime = time.strftime("%H:%M:%S", t)
     
-    if srvGpsd.IsRunning():
-        gpsStatus = luna_core.getGpsdFixType()
-    else:
-        gpsStatus = '   '
-
-    if srvChrony.IsRunning():
-        ntpStatus = 'NTP'
-    else:
-        ntpStatus = '   '
-
-    lcd.cursor_position(0, 0)
-    lcd.message = gpsStatus
-    lcd.cursor_position(12, 0)
-    lcd.message = strTime
-    lcd.cursor_position(0, 1)
-    lcd.message = ntpStatus
-    time.sleep(1)
+  if srvChrony.IsRunning():
+      ntpStatus = 'NTP'
+  else:
+      ntpStatus = '   '
+  lcd.cursor_position(0, 0)
+  lcd.message = get_gps_status()[0]
+  led.value = get_gps_status()[1]
+  lcd.cursor_position(12, 0)
+  lcd.message = strTime
+  lcd.cursor_position(0, 1)
+  lcd.message = ntpStatus
+  time.sleep(1)
--- luna_core.py
+++ luna_core.py
@@ -1,16 +1,27 @@
 # System checks for the status LCD
 
-from typing import Text
-from pystemd.systemd1 import Unit
 import gpsd
+import logging
+from pystemd.systemd1 import Unit
 
-gpsd.connect()
+# Create logger
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+# Create file handler
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+file_handler = logging.FileHandler('luna-ui.log')
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
+
 
 def getGpsdFixType():
   try:
+    gpsd.connect()
     return gpsd.get_current().mode
   except:
-    print('Exception - GPSD not running?')
+    logger.warn('Connection to gpsd failed, retrying...')
+  
   
 # Wrapper class for pystemd
 class LinuxService:
@@ -18,6 +29,7 @@ class LinuxService:
     serviceName = name + '.service'
     self.service = Unit(bytes(serviceName,'utf8'))
     self.service.load()
+    logger.info('LinuxService has loaded service %s', self.service)
 
   def IsRunning(self):
     return self.service.Unit.ActiveState.decode() == 'active'
