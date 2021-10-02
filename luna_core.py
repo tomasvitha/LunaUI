d5bda77081265a228e0622206afbd614626c172c
Tomas Vitha <fakir@hotmail.cz>
2021-10-02 22:48:39 +0200

    Added chrony server stats handling

--- luna_core.py
+++ luna_core.py
@@ -2,6 +2,7 @@
 
 import gpsd
 import logging
+import subprocess
 from pystemd.systemd1 import Unit
 
 # Create logger
@@ -14,18 +15,32 @@ file_handler = logging.FileHandler('luna-ui.log')
 file_handler.setFormatter(formatter)
 logger.addHandler(file_handler)
 
-
+# Wrapper for gpsd object
 def getGpsdFixType():
   try:
     gpsd.connect()
     return gpsd.get_current().mode
   except:
     logger.warn('Connection to gpsd failed, retrying...')
+
+# Chrony server statistics
+class ChronyServerStats:
+  def __init__(self) -> None:
+    self.ntp_packets_last = 0
+    self.ntp_packets = 0
+    self.ntp_dropped = 0
+    self.reqs_per_second = 0
   
-  
+  def get_stats(self):
+    raw_result = subprocess.run(['chronyc', '-c', 'serverstats'], stdout = subprocess.PIPE) 
+    stats = raw_result.stdout.decode('utf8').split(',')
+    self.ntp_packets = stats[0]
+    self.ntp_dropped = stats[1]
+    self.reqs_per_second = self.ntp_packets - self.ntp_packets_last
+
 # Wrapper class for pystemd
 class LinuxService:
-  def __init__(self, name):
+  def __init__(self, name) -> None:
     serviceName = name + '.service'
     self.service = Unit(bytes(serviceName,'utf8'))
     self.service.load()
@@ -33,3 +48,4 @@ class LinuxService:
 
   def IsRunning(self):
     return self.service.Unit.ActiveState.decode() == 'active'
+
