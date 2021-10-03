92518611c2a5c5204435f36db44ec52699126752
Tomas Vitha <fakir@hotmail.cz>
2021-10-03 14:40:47 +0200

    Moved text functions to classes in core module. Changed the loop sleep function to be more stable.

--- luna-ui.py
+++ luna-ui.py
@@ -9,85 +9,54 @@ import netifaces as ni
 lcd = luna_hardware.lcd
 led = luna_hardware.led
 
-# get service objects
-srvGpsd = luna_core.LinuxService('gpsd')
-srvChrony = luna_core.LinuxService('chrony')
-chrony_stats = luna_core.ChronyServerStats()
-
 # get logger
 logger = logging.getLogger(__name__)
 logger.addHandler(luna_core.file_handler)
 
+# get services from core module
+srv_chrony = luna_core.LinuxService('chrony')
+srv_gpsd = luna_core.LinuxService('gpsd')
+gps_stats = luna_core.GpsdServiceStatus(srv_gpsd)
+ntp_stats = luna_core.ChronyServerStats(srv_chrony)
+
 # clear the LCD and LED on exit
-def clear_hw():
+def exit():
     lcd.clear()
     led.value = False
-atexit.register(clear_hw)
+atexit.register(exit)
 
 # LED test and welcome screen
-time.sleep(0.1)
 lcd.message = '12345678901234567890\n********************'
 led.value = True
-time.sleep(3)
+time.sleep(1)
 led.value = False
 
 # Show hostname and IP address
 ipAddress = ni.ifaddresses('eth0')[ni.AF_INET][0]['addr']
 lcd.clear()
 lcd.message = 'luna.vitha.cz\n' + ipAddress
-time.sleep(4)
+time.sleep(1)
 lcd.clear()
 
-# Prepare GPS strings
-def format_gps_status():
-    if srvGpsd.IsRunning():
-        fix_type = luna_core.getGpsdFixType()
-        text = ''
-        led_status = False
-        if fix_type == 1:
-            text = 'GPS NO FIX'
-            led_status = False
-        elif fix_type == 2:
-            text = 'GPS 2D    '
-            led_status = not led.value # blink at polling rate
-        elif fix_type == 3:
-            text = 'GPS 3D    '
-            led_status = True
-    else:
-        text = 'NO GPS    '
-        led_status = False
-
-    return text, led_status
-
-# Prepare NTP strings
-count = 0
-def format_ntp_status():
-    global count
-    if srvChrony.IsRunning():
-        chrony_stats.get_stats()
-        # we have multiple screens to show
-        if count < 3: ntp_text = f'NTP pps: {chrony_stats.reqs_per_second}'.ljust(20)
-        elif count < 6: ntp_text = f'NTP packets: {chrony_stats.ntp_packets}'.ljust(20)
-        elif count < 9: ntp_text = f'NTP dropped: {chrony_stats.ntp_dropped}'.ljust(20)
-        
-        count += 1
-        if count >= 9: count = 0
-    else: 
-        ntp_text = 'NTP offline'.ljust(20)
-
-    return ntp_text
-
-
-# show clock (temp)
 while True:
-  t = time.strftime("%H:%M:%S", time.localtime())
-    
-  lcd.cursor_position(0, 0)
-  lcd.message = format_gps_status()[0]
-  led.value = format_gps_status()[1]
-  lcd.cursor_position(12, 0)
-  lcd.message = t
+    next_time = time.time() + 1
 
-  lcd.cursor_position(0, 1)
-  lcd.message = format_ntp_status()
-  time.sleep(1)
+    # show current time
+    lcd.cursor_position(12, 0)
+    lcd.message = time.strftime("%H:%M:%S", time.localtime())
+
+    # Refresh GPS status
+    gps_stats.update()
+    ntp_stats.update()
+
+    # show GPS text + set LED value
+    lcd.cursor_position(0, 0)
+    lcd.message = gps_stats.gps_text
+    led.value = gps_stats.led_status
+
+    # show current NTP text
+    lcd.cursor_position(0, 1)
+    lcd.message = ntp_stats.ntp_text
+
+    # sleep until next second (drifts a bit but who cares)
+    time.sleep(max(0, next_time - time.time()))
